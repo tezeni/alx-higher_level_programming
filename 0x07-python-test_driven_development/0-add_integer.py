92db6d08b469797e490c5685a865b4681fb7f872
tezeni <teztig@gmail.com>
2023-10-30 00:24:59 +0300

    python files

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" this file will add integers. """
+
+def add_integer(a, b=98):
+    """ returns integer addition of a and b. """
+    if ((not isinstance(a, int) and not isinstance(a, float))):
+        raise TypeError("a must be an integer")
+    if (((not isinstance(b, int) and not isinstance(b, float))):
+        raise TypeError("a must be an integer")
+    return (int (a) + int (b))
