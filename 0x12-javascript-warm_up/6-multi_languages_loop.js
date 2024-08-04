511c5c66f4e7a111ab0d9260a8384d7ead1a35f9
tezeni <teztig@gmail.com>
2024-08-04 04:15:25 -0700

    6

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+/*  prints 3 lines: (like 1-multi_languages.js) but by using an array
+of string and a loop
+*/
+
+const array = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+
+for (let i = 0; i < array.length; i++) {
+  console.log(array[i]);
+}
